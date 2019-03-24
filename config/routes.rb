cbd4d523930d9cc456526770a4f8cc3e9899329e
“Marksoloff” <marksoloff@gmail.com>
2019-03-23 21:50:39 -0500

    gemfile update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/index" => "actions#index"
-  post "/actions" => "actions#create"
-  get "/actions/:id" => "actions#show"
+  get '/' => 'actions#index'
+  post '/actions' => 'actions#create'
+  get '/actions/:id' => 'actions#show'
 end
