cc744b6c414729e9792792ff7d6f165ece65a598
“Marksoloff” <marksoloff@gmail.com>
2019-04-01 20:38:24 -0500

    updates

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -3,12 +3,12 @@ class ActionsController < ApplicationController
   def index
     @services = Service.all
     @events = Event.all
-    render 'index.erb'
+    render 'actions/index.html.erb'
   end
 
-  def calendar
-    render 'actions/calendar.erb'
-  end
+  # def calendar
+  #   render 'actions/calendar.erb'
+  # end
 
 
   # def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
+
+    Event.create(title:'Long titled event wahooo' ,
+                  start: 'Monday afternoon',
+                  end: 'Tuesday morning'
+                )
+
+
+    Event.create(title:'Short 1' ,
+                  start: '01/01/2019',
+                  end: '01/02/2019'
+                )
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
